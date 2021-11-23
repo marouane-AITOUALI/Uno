fbbe03ab9f066b63857f1b05672fd7c8d81ef3d2
aitouali1u <aitouali1u@etu.univ-lorraine.fr>
2021-11-23 13:18:14 +0100

    ecrire(fichier) / abstract getNom

--- src/cartes/Chiffre.java
+++ src/cartes/Chiffre.java
@@ -23,4 +23,24 @@ public class Chiffre extends Carte {
     public boolean peutEtrePoseeSur(Chiffre c){
         return this.valeur == c.getValeur();
     }
+
+    public boolean peutEtrePoseeSur(Plus2 c){
+        return false;
+    }
+
+    public boolean peutEtrePoseeSur(Plus4 c){
+        return false;
+    }
+
+    public boolean peutEtrePoseeSur(ChangementDeSens c){
+        return false;
+    }
+
+    public boolean peutEtrePoseeSur(PasseTonTour c){
+        return false;
+    }
+
+    public boolean peutEtrePoseeSur(Joker c){
+        return false;
+    }
 }
