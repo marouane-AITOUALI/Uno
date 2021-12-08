a95042840860b996398b4b52df3797882e3de402
aitouali1u <marouane.ait-ouali5@etu.univ-lorraine.fr>
2021-12-08 15:08:07 +0100

    Modification class Uno

--- src/cartes/Uno.java
+++ src/cartes/Uno.java
@@ -1,7 +1,44 @@
 package cartes;
 
+import java.util.ArrayList;
+
 public class Uno {
+
+    private int joueurActuel;
+    private int joueurQuiDistribue;
+    private PaquetDeCartes pioche;
+    private PaquetDeCartes talon;
+    private boolean sensPartie;
+    private ArrayList<Joueur> joueurs;
+
     public Uno(){
+        initialiser(joueurs.size());
+    }
+
+    public void creerJoueur(int nbJoueur){
+        this.joueurs = new ArrayList<>(nbJoueur);
+    }
+
+    public void distribuerCartes(){
 
     }
+
+    public void choisirQuiDistribue(){
+
+    }
+
+    public void choisirQuiJoue(){
+
+    }
+
+
+
+    public void initialiser(int nbJoueur){
+        assert (nbJoueur > 1) : "Bug nombre de joueurs";
+        this.creerJoueur(nbJoueur);
+        this.distribuerCartes();
+
+    }
+
+
 }
